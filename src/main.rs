3a493cfae6884d3eaaa75c0beee19b9be93f059c
Alexander Koterev <kotexander@gmail.com>
2022-08-27 21:28:53 -0400

    added basic window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+mod game;
+use game::*;
+
+struct App {
+    game: Game
+}
+impl App {
+    fn new() -> Self {
+        let game = Game::new();
+
+        Self { game }
+    }
+}
+
+#[macroquad::main("Power Crisis")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        
+        next_frame().await
+    }
 }
