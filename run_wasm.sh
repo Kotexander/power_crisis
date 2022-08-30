bcd130a4106a089c3ef351375bced23ef80565b4
Alexander Koterev <kotexander@gmail.com>
2022-08-29 21:04:26 -0400

    run wasm now opens game in browser for you

--- run_wasm.sh
+++ run_wasm.sh
@@ -1,3 +1,8 @@
 cargo build --target wasm32-unknown-unknown
 cp ./target/wasm32-unknown-unknown/debug/power_crisis.wasm .
+if [[ $OSTYPE == 'linux'* ]]; then 
+    xdg-open http://127.0.0.1:4000
+elif [[ $OSTYPE == 'linux'* ]]; then 
+    start http://127.0.0.1:4000
+fi
 basic-http-server .
