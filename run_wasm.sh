f059f56031e090220d5b0b01349f2150ca80078c
TurtleKhight <tankbot344lol@gmail.com>
2022-08-29 21:10:17 -0400

    fixed run wasm script for windows

--- run_wasm.sh
+++ run_wasm.sh
@@ -2,7 +2,7 @@ cargo build --target wasm32-unknown-unknown
 cp ./target/wasm32-unknown-unknown/debug/power_crisis.wasm .
 if [[ $OSTYPE == 'linux'* ]]; then 
     xdg-open http://127.0.0.1:4000
-elif [[ $OSTYPE == 'linux'* ]]; then 
+elif [[ $OSTYPE == 'msys'* ]]; then 
     start http://127.0.0.1:4000
 fi
 basic-http-server .
